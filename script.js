a3e37f70155e58891ae5b317bac804b7ccaf56e3
hmquannnnn <minhquanhoang040103@gmail.com>
2026-02-21 19:55:43 +0700

    hehe

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "./love-confession.png",    // 0 normal
+    "./confused.png",  // 1 confused
+    "./pleading.png",             // 2 pleading
+    "./sad.png",             // 3 sad
+    "./sadder.png",       // 4 sadder
+    "./devastated.png",             // 5 devastated
+    "./very-devastated.png",               // 6 very devastated
+    "./crying-runaway.png"  // 7 crying runaway
 ]
 
 const noMessages = [
